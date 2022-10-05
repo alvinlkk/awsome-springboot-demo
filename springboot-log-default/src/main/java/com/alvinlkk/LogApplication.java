b70a869f59c341cb8723a7455e85fc5eb73e0b5d
alvinlkk <332059317@qq.com>
2022-10-05 18:27:29 +0800

    logback

--- springboot-log-default/src/main/java/com/alvinlkk/LogApplication.java
+++ springboot-log-default/src/main/java/com/alvinlkk/LogApplication.java
@@ -3,15 +3,24 @@ package com.alvinlkk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@Slf4j
 public class LogApplication
 {
     public static void main( String[] args )
     {
         SpringApplication.run(LogApplication.class, args);
+
+        log.trace("Trace 日志...");
+        log.debug("Debug 日志...");
+        log.info("Info 日志...");
+        log.warn("Warn 日志...");
+        log.error("Error 日志...");
     }
 }
